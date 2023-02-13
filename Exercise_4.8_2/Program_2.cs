76271f61ae6d936004fbfb1b01fa05c2d77433f2
Catherine <katya.simonova.1997@mail.ru>
2023-02-13 23:39:58 +0300

    Добавьте файлы проекта.

---
+++ Exercise_4.8_2/Program_2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4._8_2
+{
+	internal class Program_2
+	{
+		static void Main(string[] args)
+		//Сложение матриц
+		{
+			Random r = new Random();
+
+			WriteLine("Матрица A и В:");
+			WriteLine("Введите кол-во строк:");
+			int x1 = int.Parse(ReadLine());
+			WriteLine("Введите коль-во столбцов:");
+			int y1 = int.Parse(ReadLine());
+			
+			int[,] matrixA = new int[x1,y1];
+			int[,] matrixB = new int[x1,y1];
+
+			WriteLine("Матрица А:");
+
+			for (int i = 0; i < x1; i++)
+			{
+				for (int j = 0; j < y1; j++)
+				{
+					matrixA[i, j] = r.Next(10);
+					Write($"{matrixA[i, j]} ");
+
+				}
+				WriteLine();
+			}
+			WriteLine("Матрица В:");
+
+			for (int k = 0; k < x1; k++)
+			{
+				for (int l = 0; l < y1; l++)
+				{
+					matrixB[k, l] = r.Next(10);
+					Write($"{matrixB[k,l]} ");
+
+				}
+				WriteLine();
+			}
+			WriteLine("Сумма матриц А и В:");
+
+			int[,] sumAB = new int[x1,y1];
+
+			for (int q = 0; q < x1; q++)
+			{
+				for (int w = 0; w <  y1	; w++)
+				{
+					sumAB[q,w] = matrixA[q,w] + matrixB[q,w];
+					Write($"{sumAB[q,w]} ");
+				}
+				WriteLine();
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4._8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		// Случайная матрица
+		//Сначала пользователю предлагается ввести количество строк в будущей матрице.
+		//Затем — ввести количество столбцов в будущей матрице.
+		//Когда данные будут получены, нужно создать в памяти матрицу заданного размера.
+		//Используя Random, заполнить матрицу случайными целыми числами.
+		//Вывести полученную матрицу на экран.
+		//Вывести сумму всех элементов этой матрицы на экран отдельной строкой.
+		{
+			Random rand = new Random();
+			int sum = 0;
+			WriteLine("Введите кол-во строк:");
+			int x = int.Parse(ReadLine());
+			WriteLine("Введите коль-во столбцов:");
+			int y = int.Parse(ReadLine());
+
+			int[,] str = new int[x , y];
+			
+
+			for (int i = 0; i < x; i++)
+			{
+				sum++;
+				for (int j = 0; j < y; j++)
+				{
+					str[i, j] = rand.Next(10);
+					sum += str[i, j];
+					Write($"{str[i, j]} ");
+				}
+				WriteLine();
+				
+			}
+			WriteLine($"\n : {sum}");
+
+		}
+	}
+}
